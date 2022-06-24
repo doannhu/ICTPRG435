b5b74136ec94154c6ec2a9483577099f577c2e31
doannhu <ndodoan@gmail.com>
2022-06-24 14:09:39 +1000

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ function validatePassword(){
         passConf.reportValidity();        
     }
 }
+// const email = document.getElementById("email");
+// email.addEventListener("change",validateEmail);
+// function validateEmail(){
+//     if 
+// }
+
 
 // Check mouse position and display in canvas. For test only!
 // canvas.addEventListener("mousemove", (event) => {
@@ -24,29 +30,24 @@ function validatePassword(){
 // });
 
 // Select line width
-const lineWid = document.getElementById("line_width");
-ctx.lineWidth = lineWid.value; 
+ctx.lineWidth = document.getElementById("line_width").value; 
 
 // Select color
-const clrP = document.getElementById("clr_p");
-const clrD = document.getElementById("clr_d");
-const clrW = document.getElementById("clr_w");
-const clrSe = document.getElementById("clr_se");
-const clrSu = document.getElementById("clr_su");
 let clr = '#4e73df';
-clrP.addEventListener("click", () =>{ clr = '#4e73df' });
-clrD.addEventListener("click", () =>{ clr = '#e74a3b' });
-clrW.addEventListener("click", () =>{ clr = '#f6c23e' });
-clrSe.addEventListener("click", () =>{ clr = '#858796' });
-clrSu.addEventListener("click", () =>{ clr = '#1cc88a' });
+document.getElementById("clr_p").addEventListener("click", () =>{ clr = '#4e73df' });
+document.getElementById("clr_d").addEventListener("click", () =>{ clr = '#e74a3b' });
+document.getElementById("clr_w").addEventListener("click", () =>{ clr = '#f6c23e' });
+document.getElementById("clr_se").addEventListener("click", () =>{ clr = '#858796' });
+document.getElementById("clr_su").addEventListener("click", () =>{ clr = '#1cc88a' });
 
 // Draw rectangular
-const recX = document.querySelector('#rec_x');
-const recY = document.querySelector('#rec_y');
-const recWidth = document.querySelector('#rec_width');
-const recHeight = document.querySelector('#rec_height');
-const recShadow = document.querySelector('#rec_shadow');
-document.querySelector('#rec_btn').addEventListener("click",() => {
+document.querySelector('#rec_btn').addEventListener("click", drawRectangular);
+function drawRectangular(){
+    const recX = document.querySelector('#rec_x');
+    const recY = document.querySelector('#rec_y');
+    const recWidth = document.querySelector('#rec_width');
+    const recHeight = document.querySelector('#rec_height');
+    const recShadow = document.querySelector('#rec_shadow');
     if(recShadow) {
         ctx.shadowColor = 'red';
         ctx.shadowOffsetX = 10;
@@ -62,38 +63,43 @@ document.querySelector('#rec_btn').addEventListener("click",() => {
         ctx.strokeStyle = clr;
         ctx.strokeRect(recX.value, recY.value, recWidth.value, recHeight.value);
      }
-});
+}
+
 
 // Draw square
-const sqX = document.querySelector('#sq_x');
-const sqY = document.querySelector('#sq_y');
-const sqSide = document.querySelector('#sq_side');
-document.querySelector('#sq_btn').addEventListener("click",() => {
+document.querySelector('#sq_btn').addEventListener("click",drawSquare);
+function drawSquare(){
+    const sqX = document.querySelector('#sq_x');
+    const sqY = document.querySelector('#sq_y');
+    const sqSide = document.querySelector('#sq_side');
     ctx.fillStyle = clr;
     ctx.fillRect(sqX.value, sqY.value, sqSide.value, sqSide.value);
     ctx.strokeStyle = clr;
-    ctx.strokeRect(sqX.value, sqY.value, sqSide.value, sqSide.value);
-});
+    ctx.strokeRect(sqX.value, sqY.value, sqSide.value, sqSide.value); 
+}
+
 
 // Draw circle
-const cirX = document.querySelector('#cir_cen_x');
-const cirY = document.querySelector('#cir_cen_y');
-const cirRad = document.querySelector('#cir_rad');
-document.querySelector('#cir_btn').addEventListener("click",() => {
+document.querySelector('#cir_btn').addEventListener("click", drawCircle);
+function drawCircle(){
+    const cirX = document.querySelector('#cir_cen_x');
+    const cirY = document.querySelector('#cir_cen_y');
+    const cirRad = document.querySelector('#cir_rad');
     ctx.beginPath();
     ctx.arc(cirX.value, cirY.value, cirRad.value, 0, 2*Math.PI);
     ctx.stroke();
     ctx.fillStyle = clr;
     ctx.fill();
-});
+}
+
 
 // Draw line
-const lineX = document.querySelector('#line_start_x');
-const lineY = document.querySelector('#line_start_y');
-const lineLen = document.querySelector('#line_len');
-const lineOpt = document.querySelector('#line_opt');
-const lineBtn = document.querySelector('#line_btn');
-lineBtn.addEventListener("click", () => {
+document.querySelector('#line_btn').addEventListener("click", drawLine);
+function drawLine(){
+    const lineX = document.querySelector('#line_start_x');
+    const lineY = document.querySelector('#line_start_y');
+    const lineLen = document.querySelector('#line_len');
+    const lineOpt = document.querySelector('#line_opt');
     ctx.beginPath();
     switch (lineOpt.value) {
         case "0":
@@ -115,9 +121,10 @@ lineBtn.addEventListener("click", () => {
             ctx.moveTo(lineX.value, lineY.value);
             ctx.lineTo(lineX.value -  lineLen.value, lineY.value);
             x.stroke();
-            break;}
+            break;}}
+
     
-});
+
 
 // Select language
 const spanBtn = document.querySelector('#spanish_btn');
